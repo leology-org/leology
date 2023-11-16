13ad9f860aef4d7c8100b205310e1d3ffc5fa5ba
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-11-16 22:15:11 +0300

    add basic testing flow blueprint

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,53 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use std::future::Future;
+use tokio::runtime::Runtime;
 
-/// Simple program to greet a person
+/// Executes the given future using a new Tokio runtime.
+pub fn block_on<F: Future>(future: F) -> F::Output {
+    let rt = Runtime::new().expect("Failed to start Tokio runtime");
+    rt.block_on(future)
+}
+
+/// Command Line Interface for the Leology Test Framework.
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+    /// Subcommands for the CLI.
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Subcommand to run tests.
+    Test,
 }
 
 fn main() {
     let args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    match args.command {
+        Commands::Test => {
+            println!("Running tests...");
+            let outcome = block_on(run_tests());
+            ensure_ok(outcome);
+        }
+    }
+}
+
+// Placeholder for the asynchronous test execution function.
+// This function should be replaced with your actual test logic.
+async fn run_tests() -> Result<(), String> {
+    // Test execution logic goes here.
+    Ok(())
+}
+
+fn ensure_ok(result: Result<(), String>) {
+    match result {
+        Ok(_) => println!("Tests completed successfully."),
+        Err(e) => {
+            println!("Error running tests: {}", e);
+            std::process::exit(1);
+        }
     }
 }
